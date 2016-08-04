c343e3552aa2b9d78b54828d32ca06c1a6a26a45
Ayyappa <ayyappa@ayyappa-linux.(none)>
2016-08-04 20:16:45 +0530

    Committing untracked changes

--- Gess/gess_colorSegmentation.py
+++ Gess/gess_colorSegmentation.py
@@ -5,6 +5,9 @@ import scipy as sp
 
 videoObj = cv.videoCapture(0)
 
+# It prompts the user to place the hand in the boundary shown
+# The hand boundary is drawn by using a sample hand5.jpg image in workspace
+# When the user places their hand, it is masked with a hand mask to retrieve the hand portion from current frame
 def getHandImage:
 
 	handImage = []
@@ -52,17 +55,39 @@ def getHandImage:
 
 	return handImage
 
-def computeMeanVariance(handImage) :
+# It computes the mean and variance of input HSV image
+# plane - parameter to select the plane => 's', 'h', 'v'
+def computeMeanVariance(handImage_hsv,plane) :
 	mean = 0
 	variance = 0
+	mean = numpy.mean(array)
+	variance = numpy.var(array) #Think if we can directly use standard deviation numpy.std
 	return mean, variance
 
+# It equalizes the luminance of the YCrCb image.
 def equalizeLuminance(image_YCrCb):
+	image_YCrCb[:,:,0] = cv.equalizeHist(image_YCrCb[:,:,0])
 	return image_YCrCb
 
+# It calculates the coordinates of the hand portion in current frame
 def getHandCoordinates(currentFrame, parameters):
+	x = 0
+	y = 0
 
+	skinBlob = detectSkinPixels(currentFrame, parameters)
+	handBlob = detectHandPixels(skinBlob)
 
+	""""# --> Helper for calculating probability from scipy import stats
+	>>> # PDF of Gaussian of mean = 0.0 and std. deviation = 1.0 at 0.
+	>>> stats.norm.pdf(0, loc=0.0, scale=1.0)
+	0.3989422804014327 """
+
+	#calculate centroid of hand blob
+
+	return [x, y]
+
+# It returns a parameter dictionary of mean and variances of each plane H,S
+# by invoking respective functions for Histogram equalization -> calculate parameters
 def getHandParameters(handImage):
 
 	handImage_ycrcb = cv.cvtColor(handImage, cv.COLOR_BGR2YCrCb)
@@ -74,7 +99,6 @@ def getHandParameters(handImage):
 	handImage_hsv = cv.cvtColor(handImage, cv.COLOR_BGR2HSV)
 
 	s_mean, s_variance = computeMeanVariance(handImage_hsv, 's')
-
 	h_mean, h_variance = computeMeanVariance(handImage_hsv, 'h')
 
 	parameterDictionary = {
@@ -90,6 +114,11 @@ def getHandParameters(handImage):
 
 	return parameterDictionary
 
+#It is used to plot the coordinates in a separate window
+def plotHandCentroid(x,y) :
+	return
+
+# It captures each frame from camera and plots the centroid of hand portion in input frame
 def detectHand():
 
 	handImage = getHandImage()
